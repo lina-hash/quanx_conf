789b6641d61810138efadd001ecf55797326ef44
Lena <linxan123@gmail.com>
2022-07-03 10:15:50 +0800

    add ting vip

---
+++ tingvip.js
@@ -0,0 +1,21 @@
+//ting version 7.0.2
+var body = $response.body;
+var obj = JSON.parse(body);
+
+var newobj = {
+    "istingvip": true,
+    "tingvipendtime": "2099-07-05T17:43:53Z",
+    "tingviptype": "OrdinaryVip",
+    "username": obj['username'],
+    "email": obj['email'],
+    "openid_type": "apple",
+    "banned": false,
+    "rechargehint": "",
+    "has_old_password": true
+}
+
+body = JSON.stringify(obj);
+
+// console.log(body);
+
+$done(body);
