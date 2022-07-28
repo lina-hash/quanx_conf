0d5aae25c3e8288dc65c9a69c4edc227b5876d0a
Lena <linxan123@gmail.com>
2022-07-28 13:45:12 +0800

    up

--- reddit.js
+++ reddit.js
@@ -2,7 +2,18 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-let edges = obj['data']['home']['elements']['edges']
+// let edges = obj['data']['home']['elements']['edges']
+
+let edges
+if ('data' in json) {
+    if ('news' in json.data)
+        edges = json.data.news.elements.edges
+    if ('home' in json.data)
+        edges = json.data.home.elements.edges
+    if ('popular' in json.data)
+        edges = json.data.popular.elements.edges
+}
+
 for (let i = 0; i < edges.length; i++) {
     // console.log(edges[i]['node']['__typename'] == 'AdPost')
     if (edges[i]['node']['__typename'] == 'AdPost') {
