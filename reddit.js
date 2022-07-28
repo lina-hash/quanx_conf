2c67399cecf6236efe78713aaf34ea8057a430b5
Lena <linxan123@gmail.com>
2022-07-28 14:01:44 +0800

    up

--- reddit.js
+++ reddit.js
@@ -1,29 +1,26 @@
 //
-var body = $response.body;
-var obj = JSON.parse(body);
+var obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
 // let edges = obj['data']['home']['elements']['edges']
 
 let edges
-if ('data' in json) {
-    if ('news' in json.data)
-        edges = json.data.news.elements.edges
-    if ('home' in json.data)
-        edges = json.data.home.elements.edges
-    if ('popular' in json.data)
-        edges = json.data.popular.elements.edges
+if ('data' in obj) {
+    if ('news' in obj.data)
+        removeAD(obj.data.news.elements.edges)
+    else if ('home' in obj.data)
+        removeAD(obj.data.home.elements.edges)
+    else if ('popular' in obj.data)
+        removeAD(obj.data.popular.elements.edges)
 }
 
-for (let i = 0; i < edges.length; i++) {
-    // console.log(edges[i]['node']['__typename'] == 'AdPost')
-    if (edges[i]['node']['__typename'] == 'AdPost') {
-        edges.splice(i, 1)
+function removeAD(edges) {
+    for (let i = 0; i < edges.length; i++) {
+        if (edges[i]['node']['__typename'] == 'AdPost') {
+            edges.splice(i, 1)
+        }
     }
 }
-obj['data']['home']['elements']['edges'] = edges
 
-body = JSON.stringify(obj);
-
+var body = JSON.stringify(obj);
 // console.log(body);
-
 $done(body);
