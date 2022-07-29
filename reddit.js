882d6151e27bacd2120382c44cc4a878469372dc
lina-hash <linxan123@gmail.com>
2022-07-29 13:59:00 +0800

    Update reddit.js

--- reddit.js
+++ reddit.js
@@ -1,28 +1,25 @@
 //
-var obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
-
-// let edges = obj['data']['home']['elements']['edges']
-
-let edges
-if ('data' in obj) {
-    if ('news' in obj.data)
-        removeAD(obj.data.news.elements.edges)
-    else if ('home' in obj.data)
-        removeAD(obj.data.home.elements.edges)
-    else if ('popular' in obj.data)
-        removeAD(obj.data.popular.elements.edges)
-    else if ('children' in obj.data && 'commentsPageAds' in obj.data.children)
-        obj.data.children.commentsPageAds = []
+if ($response.body.includes("adEvents")) {
+    var body = JSON.parse($response.body || null);
+    if (body.data.news) {
+        removeAD(body.data.news.elements)
+        $done(JSON.stringify(body));
+    } else if (body.data.home) {
+        removeAD(body.data.home.elements)
+        $done(JSON.stringify(body));
+    } else if (body.data.popular) {
+        removeAD(body.data.popular.elements)
+        $done(JSON.stringify(body));
+    } else if (body.data.children.commentsPageAds) {
+        body.data.children.commentsPageAds = []
+        $done(JSON.stringify(body));
+    } else {
+        $done();
+    }
+} else {
+    $done();
 }
 
 function removeAD(edges) {
-    for (let i = 0; i < edges.length; i++) {
-        if (edges[i]['node']['__typename'] == 'AdPost') {
-            edges.splice(i, 1)
-        }
-    }
+    elements.edges = elements.edges.filter((n) => !n.node.adEvents)
 }
-
-var body = JSON.stringify(obj);
-// console.log(body);
-$done(body);
