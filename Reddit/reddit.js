4928fb46a9113b4459d4b69d0b04b53b64923487
Lena <linxan123@gmail.com>
2022-07-30 09:12:44 +0800

    up

--- Reddit/reddit.js
+++ Reddit/reddit.js
@@ -3,21 +3,21 @@ if ($response.body.includes("adEvents")) {
     var body = JSON.parse($response.body || null);
     if (body.data.news) {
         removeAD(body.data.news.elements)
-        $done(JSON.stringify(body));
+        $done({ body: JSON.stringify(body) });
     } else if (body.data.home) {
         removeAD(body.data.home.elements)
-        $done(JSON.stringify(body));
+        $done({ body: JSON.stringify(body) });
     } else if (body.data.popular) {
         removeAD(body.data.popular.elements)
-        $done(JSON.stringify(body));
+        $done({ body: JSON.stringify(body) });
     } else if (body.data.children.commentsPageAds) {
         body.data.children.commentsPageAds = []
-        $done(JSON.stringify(body));
+        $done({ body: JSON.stringify(body) });
     } else {
-        $done();
+        $done({});
     }
 } else {
-    $done();
+    $done({});
 }
 
 function removeAD(edges) {
